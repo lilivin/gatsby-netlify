082b1a5f2f7d9b0aaae33c7075d564d35cf14d7d
KLiwin <k.liwinski@gmail.com>
2020-08-31 23:02:48 +0200

    commit

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -23,6 +23,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
             style={{
               marginTop: rhythm(1),
               marginBottom: 0,
+              color: 'pink'
             }}
           >
             {post.frontmatter.title}
