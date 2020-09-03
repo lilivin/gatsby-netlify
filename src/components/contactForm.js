7cb5ab84e6338f8371b634cfee370eab174380e8
KLiwin <k.liwinski@gmail.com>
2020-09-03 11:13:01 +0200

    form

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,24 +1,48 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCoffee, faEnvelopeOpenText} from '@fortawesome/free-solid-svg-icons'
+import {faFacebook, faInstagram} from '@fortawesome/free-brands-svg-icons'
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
 
 import { rhythm } from "../utils/typography"
-import '../styles/bio.css'
+import '../styles/contactForm.css'
 
 const Bio = () => {
   return (
     <>
-        <form
-            name="contact-form"
-            method="post"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-        >
-            <input name="name" placeholder="Your name" type="text" />
-            <input name="email" placeholder="name@name.com" type="email" />
-            <textarea name="message" />
-            <button>Send</button>
-        </form>
+      <h1 className="header">Kontakt</h1>
+      <div className="contactContainer">
+          <form
+              name="contact-form"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              className="contactForm"
+          >
+              <label className="contactForm__label">Imie</label>
+              <input name="name" placeholder="Wpisz imie..." type="text" className="contactForm__input"/>
+              <label className="contactForm__label">Email</label>
+              <input name="email" placeholder="Wpisz email..." type="email" className="contactForm__input"/>
+              <label className="contactForm__label">Wiadomość</label>
+              <textarea name="message" placeholder="Treść wiadomości..." className="contactForm__input"/>
+              <button className="contactForm__button">Send</button>
+          </form>
+          <div className="contactAlternatives">
+            <div className="contactWay">
+              <FontAwesomeIcon icon={faEnvelopeOpenText} size="2x"/>
+              <p>k.liwinski@gmail.com</p>
+            </div>
+            <div className="contactWay">
+              <FontAwesomeIcon icon={faInstagram} size="2x"/>
+              <p>@k.liwinski</p>
+            </div>
+            <div className="contactWay">
+              <FontAwesomeIcon icon={faFacebook} size="2x"/>
+              <p>@karol.liwinski.7</p>
+            </div>
+          </div>
+      </div>
     </>
   )
 }
